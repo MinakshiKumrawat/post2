521c435bea8155f7c16332ec999cd4bc4fb051c4
minakshi kumrawat <minakshikumrawat14@gmail.com>
2021-08-11 10:53:31 +0530

    update

---
+++ src/component/post/post.js
@@ -0,0 +1,41 @@
+const Post = () => {
+    return(
+        <section className="post-block post2">
+                <div className="row">
+                  <div className="col-md-12">
+                      <div className="content-section">
+                      <div className="sub-content-section">
+                          <h3>We help you grow your money</h3>
+                          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                              Lorem Ipsum has been the industry's standard dummy text ever 
+                          </p>
+                      </div>
+                      <div className="center-img">
+                        <img src="./images/three-img.png" alt="img" /> 
+                      </div>
+                      <h1>invest in <br /><b>your future</b></h1>
+                      <div className="website">
+                        <p>www.investingservices.com</p>
+                      </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-img-section">
+                <img src="./images/left-top-img.png" alt="img" className="left-top-img"/>
+                <img src="./images/right-top-spin-line.png" alt="img" className="right-top-spin-line" />
+                <img src="./images/genetic-line-img.png" alt="img" className="genetic-line-img"/>
+                <img src="./images/circle-img1.png" alt="img" className="circle-img1"/>
+                <img src="./images/circle-img2.png" alt="img" className="circle-img2"/>
+                <img src="./images/center-left-spin-line.png" alt="img" className="center-left-spin-line"/>
+                <img src="./images/coin1-img.png" alt="img" className="coin1-img"/>
+                <img src="./images/coin2-img.png" alt="img" className="coin2-img"/>
+                <img src="./images/bottom-left-spin-line.png" alt="img" className="bottom-left-spin-line"/>
+                <img src="./images/bottom-right-spin-line.png" alt="img" className="bottom-right-spin-line"/>
+                <img src="./images/circle-img3.png" alt="img" className="circle-img3"/>\
+                <img src="./images/circle-img4.png" alt="img" className="circle-img4"/>
+              </div>
+        </section>
+    )
+}
+
+export default Post;
